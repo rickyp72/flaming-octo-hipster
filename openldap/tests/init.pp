c9df15ef0b77fb61aca96c10dd6c295840ad5032
ricky <rickyp72@googmail.com>
2014-03-10 15:44:14 +0000

    update

--- openldap/tests/init.pp
+++ openldap/tests/init.pp
@@ -1,11 +1,9 @@
 # sudo puppet apply /home/ricky/.puppet/modules/openldap/tests/init.pp --modulepath="/home/ricky/.puppet/modules/" --debug
 
-# class { 'openldap::server':
-# }
-include openldap
+class { 'openldap::server':}
 
 openldap::server::database { 'dc=example,dc=com':
-  directory => '/var/lib/ldap',
+  #directory => '/var/lib/ldap',
   rootdn    => 'cn=admin,dc=example,dc=com',
   rootpw    => openldap_password('mySuperSecretPassword'),
 
